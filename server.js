b97270c174777ad87678d54664665a0d1d39552d
MasonMarc <masonmarcantel@gmail.com>
2023-01-05 16:56:42 -0500

    test

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send(path.join(__dirname, './public/index.html'));
 });
