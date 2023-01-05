c6eca87ba2191d946a441048f90c664577bfdc9f
MasonMarc <masonmarcantel@gmail.com>
2023-01-05 16:44:12 -0500

    added index

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Note Taker');
+  res.send(path.join(__dirname, './public/index.html'));
 });
+
+
 
 app.listen(PORT);
