bca736794cc387028f7e9bc21888b5cf4dd75019
MasonMarc <masonmarcantel@gmail.com>
2023-01-05 17:02:04 -0500

    made notes path work

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const path = require('path');
 
 app.use(express.static('public'));
 
+// send index file to / path
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// send notes file to /notes path
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
